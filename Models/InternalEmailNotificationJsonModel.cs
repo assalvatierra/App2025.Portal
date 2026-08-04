03cdc583a32b653218d2c4b3db2dd115dd72c5ce
Jahdiel Villosa <jahdielsvillosa@gmail.com>
2026-08-04 13:46:35 +0800

    Add Model for InternalEmailNofification

---
+++ Models/InternalEmailNotificationJsonModel.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Portal.Models
+{
+    public class InternalEmailNotificationJsonModel
+    {
+        public string InternalNotificationEmail { get; set; }
+        public string InternalNotificationEmailSubject { get; set; }
+        public string InternalNotificationEmailTitle { get; set; }
+        public string InternalNotificationEmailMessage { get; set; }
+        public string ProceedToPayment { get; set; }
+
+        [JsonConverter(typeof(InternalEmailNotificationJsonModelStringConverter))]
+        public string InternalNotificationEmails { get; set; }
+    }
+
+
+
+    public class InternalEmailNotificationJsonModelStringConverter : JsonConverter<string>
+    {
+        public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            using var jsonDoc = JsonDocument.ParseValue(ref reader);
+            return jsonDoc.RootElement.GetRawText();
+        }
+
+        public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+        {
+            using var document = JsonDocument.Parse(value);
+            document.RootElement.WriteTo(writer);
+        }
+    }
+
+}
